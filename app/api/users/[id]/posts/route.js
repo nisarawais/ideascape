e666e8492cba7f8508785bb47a64821e2bbb2dd7
aanisar <55286102+aanisar@users.noreply.github.com>
2023-06-26 20:29:28 -0400

    filtered the post by username in the profile page

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,14 +1,16 @@
 import Idea from "@models/idea";
 import { connectToDB } from "@utils/database";
 
-export const GET = async (request) => {
+export const GET = async(request, {params}) => {
     try {
-        await connectToDB()
-
-        const ideas = await Idea.find({}).populate('creator')
-
-        return new Response(JSON.stringify(ideas), { status: 200 })
+        await connectToDB();
+        const ideas = await Idea.find({
+            creator: params.id
+        }).populate('creator');
+        return new Response(JSON.stringify(ideas), {
+            status:200})
     } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
+        return new Response("Failed to fetch all ideas", {
+        status:500})
     }
-} 
+}
